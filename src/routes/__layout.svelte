6c7dd3d40a11da8657ea9e36e3888496d7b53c5a
Chris de la Iglesia <cmdli@Chriss-MacBook-Pro.local>
2022-02-22 20:26:49 -0800

    Add down arrow and basic bot assets

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,9 +2,15 @@
 	import '../app.css';
 </script>
 
-<div class="navbar bg-base-100 flex justify-between items-center space-x-4">
-	<h1 class="h">RSABots</h1>
-	<a class="btn" href="/about">About</a>
-</div>
+<svelte:head>
+	<title>RSABots</title>
+</svelte:head>
 
-<slot />
+<div class="flex flex-col w-full h-full">
+	<div class="navbar bg-base-100 flex justify-between items-center space-x-4">
+		<h1 class="h">RSABots</h1>
+		<a class="btn" href="/about">About</a>
+	</div>
+
+	<slot />
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,19 +2,18 @@
 	import Layout from './__layout.svelte';
 </script>
 
-<svelte:head>
-	<title>Test</title>
-</svelte:head>
-
 <div class="flex flex-col items-center w-full h-full">
-	<div class="mt-96 flex flex-col items-center max-w-[48rem] w-[48rem] grow">
-		<div class="max-w-[24rem] space-y-4 flex flex-col align-center">
+	<div class="mt-96 flex flex-col items-center grow">
+		<div class="w-[24rem] max-w-full space-y-4 flex flex-col align-center">
 			<div>Robot</div>
 			<textarea class="textarea textarea-info" placeholder="Enter Public Key..." />
 			<div class="flex justify-center space-x-4">
 				<div class="btn btn-primary">Random</div>
 				<div class="dropdown">
-					<button class="btn btn-primary">Download</button>
+					<button class="btn btn-primary"
+						>Download
+						<div class="arrow" /></button
+					>
 					<ul tabindex="0" class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
 						<li><a href="/">SVG</a></li>
 						<li><a href="/">PNG</a></li>
@@ -22,9 +21,25 @@
 				</div>
 			</div>
 		</div>
-		<div class="mt-4 grow">
+		<div class="mt-36 w-[36rem] max-w-full">
 			<h2 class="mb-0 text-4xl">How It Works</h2>
 			<p class="mt-0">Lorem ipsum</p>
 		</div>
 	</div>
 </div>
+
+<style>
+	.arrow {
+		height: 0.5rem;
+		width: 0.5rem;
+		content: '';
+		transform-origin: 75% 75%;
+		transform: rotate(45deg) translateY(-0.3rem);
+		box-shadow: 2px 2px;
+		pointer-events: none;
+		box-sizing: border-box;
+		border-width: 0;
+		border-style: solid;
+		border-color: #e5e7eb;
+	}
+</style>
