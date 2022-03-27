1a4d3e5eafc76daa07effd9b24a7050983eac35d
Chris de la Iglesia <chris@cmdli.dev>
2022-03-26 18:51:35 -0700

    Update script to use dry run

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-ssh rsabots.com '
+if [[ $1 = "--dry-run=0" ]] 
+then
+    ssh rsabots.com '
 
-cd /home/ec2-user/rsabots
-git pull origin master
-npm run build
-sudo systemctl restart rsabots.service
+    cd /home/ec2-user/rsabots
+    git pull origin master
+    npm run build
+    sudo systemctl restart rsabots.service
 
-'
+    '
+else
+    echo 'Run with --dry-run=0 to do anything.'
+fi
